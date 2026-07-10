82751a92c1165142c0a243cbd6b657507977700e
JohnWesley14 <johnwesleyrvaz@gmail.com>
2026-07-10 17:38:23 -0300

    mascaras adicionadas

--- index.php
+++ index.php
@@ -1,29 +1,90 @@
-<form action="" method="get">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
     <input type="button" value="Home" onclick="window.location.href = '../regex'">
     <input type="text" name="name" autocomplete="off" placeholder="Nome">
-    <input type="number" name="cpf" autocomplete="off" placeholder="cpf: XXX.XXX.XXX-XX" maxlength="11" >
+    <input type="text" name="cpf" class="cpf" autocomplete="off" placeholder="cpf: XXX.XXX.XXX-XX" maxlength="14" >
     <input type="text" name="email" autocomplete="off" placeholder="email">
-    <input type="text" name="telefone" autocomplete="off" placeholder="telefone: (XX) XXXX-XXXX">
+    <input type="text" name="telefone" class="telefone" autocomplete="off" placeholder="telefone: (XX) XXXX-XXXX" maxlength="15">
     <input type="submit" value="enviar">
 </form>
+<script>
+    const inputCPF = document.querySelector(".cpf")
+    const inputTelefone = document.querySelector(".telefone")
+
+    inputCPF.addEventListener("input", (e) => {
+        
+        e.target.value = e.target.value.replace(/[^0-9\.-]/g, "")
+
+        const apagando = ((e.inputType == "deleteContentBackward") || (e.inputType == "deleteContentForward"))
+        console.log(apagando);
+        console.log(e.target.value.length)
+        if(!apagando){
+            if(e.target.value.length == 3 || e.target.value.length == 7){
+                e.target.value = e.target.value + '.'
+            }
+            if(e.target.value.length == 11){
+                
+                e.target.value = e.target.value + '-'
+            }
+        }{
+        }
+    }); 
+
+    inputTelefone.addEventListener("input", (e) =>{
+        console.log((/[0-9]\(\)/g.test(e.target.value)))
+        e.target.value = e.target.value.replace(/[^0-9\(\)\- ]/g, "")
+
+        const apagando = ((e.inputType == "deleteContentBackward") || (e.inputType == "deleteContentForward"))
+        console.log(apagando);
+        console.log(e.target.value.length)
+        if(!apagando){
+            
+            if(e.target.value.length == 1){
+                e.target.value = `(${e.target.value}`
+            }
+            if(e.target.value.length == 3){
+                e.target.value = e.target.value + ')'
+            }
+            if(e.target.value.length == 4){
+                e.target.value = e.target.value + ' '
+            }
+            if(e.target.value.length == 5){
+                e.target.value = e.target.value + '9'
+            }
+            if(e.target.value.length == 10){
+                
+                e.target.value = e.target.value + '-'
+            }if(e.target.value > 10) {e.target.value = e.target}
+        }{
+        }
+
+    })
+
+    
+        
+</script>
+</body>
+</html>
+
 <?php
-// [^ ... ] = Significa "Tudo que NÃO seja o que está aqui dentro"
-// \p{L}    = Qualquer letra (com ou sem acento)
-// \d       = Qualquer número
-// \s       = Espaços em branco
-// /u       = Pra aparecer os acentos
-// 
-$regexName = "/[^a-zA-Z0-9\p{L}\s]/u";
+
+$regexName = "/[^a-zA-Z\p{L}\s]/u";
 $regexCPF = "/^([0-9]{3}\.){2}([0-9]{3})(\-[0-9]{2})$/";
 $regexCPFJustNumbers = "/^[0-9]{11}$/";
 $regexEmail = "/([a-z0-9]{2,})@([a-z0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?/";
-$regexTelefone = "/\(\d{2}\).?(9\d{4}-\d{4})/";
+$regexTelefone = "/^\(\d{2}\).?(9\d{4}-\d{4})$/";
 
 if (isset($_GET['email']) && $_GET['email'] != '') {
     $text = $_GET['email'];
 
     print("<hr>");
-    print("<br>");
     if (preg_match($regexEmail, $text)) {
         print ("Email: " . $text);
     } else {
@@ -36,18 +97,20 @@ if (isset($_GET['name']) && $_GET['name'] != '') {
 
 
     print ("<hr>");
-    print ("<br>");
     if(preg_match($regexName, $text)){
         $text = preg_replace($regexName, '',$text);
         print("Nome inválido, retire caracteres especiais");
         }else{
         print ("Nome: " . $text);
+        
     }
 }
 
 
 if (isset($_GET['cpf']) && $_GET['cpf'] != '') {
     $text = $_GET['cpf'];
+
+    print ("<hr>");
     if (preg_match($regexCPF, $text)) {
         echo "CPF: " . $text;
     } else if (preg_match($regexCPFJustNumbers, $text)) {
@@ -62,15 +125,18 @@ if (isset($_GET['cpf']) && $_GET['cpf'] != '') {
 
 if(isset($_GET['telefone']) && $_GET['telefone'] != ''){
     $text = $_GET['telefone'];
-    if(preg_match($regexTelefone, $text)){
     
-    }else if(preg_match("/\d{11}/", $text)){
+    print ("<hr>");
+    if(preg_match($regexTelefone, $text)){
+        echo "Número: " . $text;
+    }else if(preg_match("/^\d{11}$/", $text)){
        $telefoneFormatado = preg_replace("/(\d{2})(\d{5})(\d{4})/", "($1) $2-$3", $text);
         echo "Nº formatado: " . $telefoneFormatado;
     }
     else{
-        echo "Número errado";
+        echo "Número errado! Siga o padrão (XX) 9XXXX-XXXX";
     }
+    print ("<hr>");
 }
 
 // <script>alert("Esqueceu de me bloquear na Cazé TV")</script>
